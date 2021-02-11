1b582faa6d5a1661c8badbd637956f84bbe0a335
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2021-02-11 09:11:27 +0100

    plot script

--- MVA/python/regression_example.py
+++ MVA/python/regression_example.py
@@ -1,3 +1,9 @@
+import ROOT
+import Analysis.Tools.syncer as syncer
+c1 = ROOT.TCanvas() # do this to avoid version conflict in png.h with keras import ...
+c1.Draw()
+c1.Print('/tmp/delete.png')
+
 import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 #argParser.add_argument('--sample',             action='store', type=str,   default='ttG_noFullyHad_fast')
@@ -12,6 +18,9 @@ import TMB.Tools.logger as logger
 logger = logger.get_logger("INFO", logFile = None )
 
 filename = "/eos/vbc/user/robert.schoefbeck/TMB/ttG_noFullyHad_fast.root"
+
+# MVA configuration
+import TMB.MVA.configs  as configs 
 
 #config
 config = getattr( configs, args.config)
@@ -30,42 +39,41 @@ model_path = '.'
 # for the plots
 save_path = '.'
 
-n_var_input = len(variables)
 
 # input samples
 import TMB.Samples.pp_TTGammaEFT as samples
 sample = samples.ttG_noFullyHad_fast
 
 # fix random seed for reproducibility
-seed = 7
-np.random.seed(seed)
-
-
-    #branches = uproot['Events'].arrays(namedecode='utf-8')
-    #basic    = (branches['PhotonGood0_pt'] >= 0 ) & (branches['LeptonTight0_pt'] >= 0)
+np.random.seed(1)
 
 regression_target = "FI_ctZ_SM"
 
-mva_variables = config.mva_variables.keys()
+#mva_variables = config.mva_variables.keys()
+
+mva_variables = [
+    #'mva_photonJetdR',
+    #'mva_photonLepdR',
+    #'mva_photon_eta',
+    'mva_photon_pt']
+
 mva_variables.sort()
+n_var_input   = len(mva_variables)
 
 upfile = uproot.open(filename)
-df     = upfile[key]["Events"].pandas.df(branches = mva_variables+[regression_target])
+df     = upfile["Events"].pandas.df(branches = mva_variables+[regression_target])
 
-## check for NaN in the dataframe, .root file might be slightly broken
-#for key in key_list:
-#    for var in config.mva_variables:
-#        if df[key][var].isnull().values.any() == True:
-#            print(key,':', var, ' has some entries as nan:')
-#            print(df[key][var].isnull().sum(), ' are nan')
-#            print('nan Events will be removed')
+#branches = upfile['Events'].arrays(namedecode='utf-8')
+#basic    = (branches['mva_m3'] >= 0 ) 
 
-df_all = df_all.dropna() # removes all Events with nan
+df = df.dropna() # removes all Events with nan -> amounts to M3 cut
 
 # split dataset into Input and output data
-dataset = df_all.values
-X = dataset[:,0:len(mva_variables)]
-Y = dataset[:,len(mva_variables)]
+dataset = df.values
+X = dataset[:,0:n_var_input]
+#Y = np.log(dataset[:,n_var_input])
+Y = np.log(dataset[:,n_var_input])
+Y[Y < -15] = -15
 
 # split data into train and test, test_size = 0.2 is quite standard for this
 from sklearn.model_selection import train_test_split
@@ -83,7 +91,7 @@ from keras.utils import np_utils
 model = Sequential()
 model.add(BatchNormalization(input_shape=(n_var_input, )))
 
-layers = [n_var_input*5, n_var_input*5, n_var_input*5]
+layers = [n_var_input+20]
 for dim in layers:
     model.add(Dense(dim, activation='sigmoid'))
 
@@ -94,7 +102,7 @@ model.summary()
 
 # define callback for early stopping
 import tensorflow as tf
-callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=1) # patience can be higher if a more accurate result is preferred
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10) # patience can be higher if a more accurate result is preferred
                                                                         # I would recommmend at least 3, otherwise it might cancel too early
 
 # train the model
@@ -115,4 +123,10 @@ model.save(model_path + '_regression_model.h5')
 
 #########################################################################################
 # Apply the model
-print(model.predict(X_test[9:10]), Y_test[9:10])
+
+import array
+x,y = pickle.load(file('tmp.pkl'))
+c = ROOT.TCanvas()
+g = ROOT.TGraph(len(Y_test), Y_test, model.predict(X_test)) 
+g.Draw('ap')
+c.Print("/mnt/hephy/cms/robert.schoefbeck/www/TMB/MVA/scatter.png")
