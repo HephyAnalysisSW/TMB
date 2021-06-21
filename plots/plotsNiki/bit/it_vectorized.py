ceb00056f5b9428af9fa35d8daad92d456ab7f11
Nikolaus Frohner <nfrohner@ac.tuwien.ac.at>
2021-06-21 07:49:24 +0200

    use new factors methods from analysis to calculate FI

--- plots/plotsNiki/bit/it_vectorized.py
+++ plots/plotsNiki/bit/it_vectorized.py
@@ -233,17 +233,23 @@ import awkward
 import numpy as np
 import pandas as pd
 
+max_events  = 100000
 input_file  = "/eos/vbc/user/robert.schoefbeck/TMB/bit/MVA-training/ttG_WG_small/WGToLNu_fast/WGToLNu_fast.root"
+#input_file  = "/scratch-cbe/users/nikolaus.frohner/TMB/bit/MVA-training/ttG_WG/WGToLNu_fast/WGToLNu_fast.root"
 upfile      = uproot.open( input_file )
 tree        = upfile["Events"]
 n_events    = len( upfile["Events"] )
-n_events    = min(10000, n_events)
+n_events    = min(max_events, n_events)
 entrystart, entrystop = 0, n_events 
 
 # Load features
 branches    = [ "mva_photon_pt", ]#"mva_photon_eta", "mva_photonJetdR", "mva_photonLepdR", "mva_mT" ]
+#branches    = [ "mva_photon_pt", "mva_photon_eta"]# "mva_photonJetdR", "mva_photonLepdR", "mva_mT" ]
 df          = tree.pandas.df(branches = branches, entrystart=entrystart, entrystop=entrystop)
 features    = df.values
+
+print(features.shape)
+assert features.shape[1] == 1
 
 # Load weights
 from Analysis.Tools.WeightInfo import WeightInfo
@@ -252,6 +258,7 @@ w.set_order(2)
 
 # Load all weights and reshape the array according to ndof from weightInfo
 weights     = tree.pandas.df(branches = ["p_C"], entrystart=entrystart, entrystop=entrystop).values.reshape((-1,w.nid))
+print(weights.shape)
 
 min_size = 50
 
@@ -259,13 +266,10 @@ assert len(features)==len(weights), "Need equal length for weights and features.
 
 FI_func = lambda coeffs: w.get_fisherInformation_matrix( coeffs, variables = ['cWWW'], cWWW=1)[1][0][0]
 # TODO: hardcoded for variables = ['cWWW'], cWWW=1
-weight_factors = np.zeros(120)
-weight_factors[0] = 1
-weight_factors[7] = 1
-weight_factors[84] = 1
-diff_weight_factors = np.zeros(120)
-diff_weight_factors[7] = 1
-diff_weight_factors[84] = 2
+weight_factors = w.get_weight_yield_factors(['cWWW'], cWWW=1)
+# calculate derivatives for all variables
+#diff_weight_factors = { var:w.get_diff_weight_yield_factors( var, cWWW=1 ) for var in ['cWWW'] }
+diff_weight_factors = w.get_diff_weight_yield_factors( 'cWWW', cWWW=1 )
 
 #node       = Node( features, weights, FI_func=FI_func, max_depth=1, min_size=min_size )
 
